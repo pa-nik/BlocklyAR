bf6605e104a515346764effe0bf6695412182eb0
pa-nik <76602480+pa-nik@users.noreply.github.com>
2023-04-03 17:22:54 -0700

    add water sheep model

--- assets/js/aframe_blocks.js
+++ assets/js/aframe_blocks.js
@@ -118,6 +118,7 @@ Blockly.defineBlocksWithJsonArray([
                     ["Kirby and Star Rod", 'assets/models/kirby_and_star_rod.glb'],
                     ["Donut", 'assets/models/donut_2.0.glb'],
                     ["Donut Half", 'assets/models/half_donut.glb'],
+                    ["Water Sheep", 'assets/models/water_sheep.glb'],
                     //["CesiumMan", 'assets/models/CesiumMan.gltf'],
                     //["Union Troop", 'assets/models/FinalUnion.glb'],
                     //["Confederate", 'assets/models/confTroop.glb'],
